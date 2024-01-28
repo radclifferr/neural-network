87c3de11c382f8ede9e7683420133c036311984f
Richard Radcliffe <107942724+radclifferr@users.noreply.github.com>
2024-01-27 19:28:17 -0500

    Fix typos

--- test.py
+++ test.py
@@ -75,7 +75,7 @@ def update_weights(gradients, weights):
 def one_training_step(model, images_batch, labels_batch):
   with tf.GradientTape() as tape:
     predictions = model(images_batch)
-    per_sample_losses = tf.keras.losses.sparse_categorical_crossentropy(labels_batch, prediction)
+    per_sample_losses = tf.keras.losses.sparse_categorical_crossentropy(labels_batch, predictions)
     average_loss = tf.reduce_mean(per_sample_losses)
   gradients = tape.gradient(average_loss, model.weights)
   update_weights(gradients, model.weights)
@@ -105,7 +105,7 @@ class BatchGenerator:
 def fit(model, images, labels, epochs, batch_size = 128):
   for epoch_counter in range(epochs):
     print(f"Epoch{epoch_counter}")
-    batch_generator = BatchGenerator(images, labels)
+    batch_generator = BatchGenerator(images, labels, batch_size)
     for batch_counter in range(batch_generator.num_batches):
       images_batch, labels_batch = batch_generator.next()
       loss = one_training_step(model, images_batch, labels_batch)
